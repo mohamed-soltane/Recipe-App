df65929372284d7f39e455956c26dff069cc6517
Soltane <soltanne92@gmail.com>
2020-04-11 21:32:31 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const  App = () => {
   const APP_KEY = "4cae2c13f8f7cfc1a80af1edae9a0e3a";
   
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
 
 
@@ -25,31 +25,34 @@ const  App = () => {
     };
     const updateSearch = e => {
      setSearch(e.target.value);
-     console.log(search);
     }
 
     const getSearch = e =>{
       e.preventDefault();
       setQuery(search);
+      setSearch('');
     }
 
 
   return (
     <div className="App">
-      <form onSubmit={getSearch} className="search_bar">
-        <input className="search-bar" type="text"  onChange={updateSearch} />
+      <form onSubmit={getSearch} className="search_form">
+        <input className="search_bar" type="text"  onChange={updateSearch} />
         <button className="search-button" type="submit" >Search</button>
 
 
       </form>
-      {recipes.map(recipe =>(
-        <Recipe 
-        title={recipe.recipe.label}
-        calories={recipe.recipe.calories}
-        image={recipe.recipe.image}
-        />
-      ))}
-        
+      <div className="recipes">
+        {recipes.map(recipe =>(
+          <Recipe 
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+
+          />
+        ))}
+      </div>   
     </div>
   );
 }
